f8eeb743d0c3c9f34cf50eb5d40a2f17040c8359
Wyndale <lapurawendel.dev@gmail.com>
2025-11-08 09:10:08 +0800

    chore: simplify assignment permission; remove unused permissions from AssignmentPermission and AuditAction enums

--- src/main/java/com/tmc/system/tmc_secure_system/entity/enums/AssignmentPermission.java
+++ src/main/java/com/tmc/system/tmc_secure_system/entity/enums/AssignmentPermission.java
@@ -1,8 +1,5 @@
 package com.tmc.system.tmc_secure_system.entity.enums;
 
 public enum AssignmentPermission {
-    VIEW_METADATA,
-    DECRYPT,
-    DOWNLOAD,
-    EXPORT
+    DECRYPT
 }
--- src/main/java/com/tmc/system/tmc_secure_system/entity/enums/AuditAction.java
+++ src/main/java/com/tmc/system/tmc_secure_system/entity/enums/AuditAction.java
@@ -3,12 +3,11 @@ package com.tmc.system.tmc_secure_system.entity.enums;
 public enum AuditAction {
     LOGIN_SUCCESS,
     LOGOUT,
-    FAILED_LOGIN,           // individual failed attempts (for audit)
+    FAILED_LOGIN,
     FILE_UPLOAD_STORED,
     FILE_UPLOAD_FAILED,
     VALIDATION_FAILED,
-    FILE_DOWNLOAD,
     DECRYPTION_SUCCESS,
     ADMIN_ACTION,
-    LOGS_EXPORTED           // compliance export action
+    LOGS_EXPORTED
 }
